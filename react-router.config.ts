1e3927cdee006d1f40355ac0fc8bad3a030a5ba4
raythurnvoid <53383860+raythurnvoid@users.noreply.github.com>
2025-02-18 23:31:39 +0000

    prepare for challenge

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-	// Disable SSR since we want static pre-rendering
+	// Config options...
+	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: false,
-	basename: "/syb-interview-ai/",
-	// Pre-render all routes at build time
-	async prerender() {
-		return ["/", "/user"];
-	},
 } satisfies Config;
