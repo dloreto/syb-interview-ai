7f37c642f9342eff2d195aeb64f26ae2257708e8
raythurnvoid <53383860+raythurnvoid@users.noreply.github.com>
2025-02-19 18:23:07 +0000

    done!

---
+++ app/routes/user.tsx
@@ -0,0 +1,11 @@
+import { useParams } from 'react-router-dom';
+
+export default function User() {
+  const { id } = useParams();
+  
+  return (
+    <div>
+      <p>User ID: {id}</p>
+    </div>
+  );
+} 
